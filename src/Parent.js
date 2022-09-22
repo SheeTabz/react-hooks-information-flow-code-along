4d64b77582fc67e0e29d154568653aebb30568a9
SheeTabz <wmuriithitabitha@gmail.com>
2022-09-22 05:40:00 +0300

    Lifting state up practice

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,16 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
-
+  const [childrenColor, setChildrenColor] = useState("FFF");
+  function handleColor(newChildrenColr){
+     const newColor = getRandomColor();
+     setColor(newColor);
+     setChildrenColor(newChildrenColr);
+  }
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleColor}/>
+      <Child color={childrenColor} onChangeColor={handleColor}/>
     </div>
   );
 }
